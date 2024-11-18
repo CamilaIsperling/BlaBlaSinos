a5251cd645a7144ee33db0e6f6062f805cd1fc3e
Camila Isperling <127119542+CamilaIsperling@users.noreply.github.com>
2024-11-18 20:42:04 -0300

    Adicionando Urls

--- BlaBlaSinos/my_app/urls.py
+++ BlaBlaSinos/my_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),  # Rota para a página de login
     path('register/', views.register_view, name='register'),
     path('', views.index, name='index'),
-    path('index_carona/', views.caronas, name='index_carona')
+    path('index_carona/', views.caronas, name='index_carona'),
+    path('index_motorista/', views.cadastrarCaronas, name='index_motorista')
 ]
